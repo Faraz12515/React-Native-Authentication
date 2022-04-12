34e26bd5a396617c49c0512128bd247284475d84
Faraz12515 <alifaraz7866@gmail.com>
2022-04-12 13:54:14 -0700

    Updates

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Home Screen</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        color: "#051C60",
+        fontSize: 26,
+        fontWeight: 'bold',
+        textAlignVertical: 'center',
+
+    }
+})
 
 export default HomeScreen
